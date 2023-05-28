bd74bebbf34619bbc395dcc95bea3eed2963e8a0
Vitor Hugo <vitorh565.vh@gmail.com>
2023-05-28 11:12:19 -0300

    .

--- tela.py
+++ tela.py
@@ -1,5 +1,6 @@
 # Imports 
 import datetime
+
 # Exceções personalizadas
 class VerificaError(Exception):
      pass
@@ -10,11 +11,11 @@ class VerificaError(Exception):
 def hora(nome):
     hora= datetime.datetime.now()
     if (hora.hour >= 4 and hora.hour < 12):
-        print(f"Bom dia {nome} !! \nComo posso ajudar hoje ? ")
+        print(f"Bom dia {nome} !! \nComo posso ajudar hoje ? \n")
     elif (hora.hour > 12 and hora.hour <=18):
-         print(f"Bom tarde {nome} !! \nComo posso ajudar hoje ? ")
+         print(f"Bom tarde {nome} !! \nComo posso ajudar hoje ? \n")
     else:
-        print("Boa noite {nome} !! \nComo posso ajudar hoje ?")
+        print("Boa noite {nome} !! \nComo posso ajudar hoje ? \n")
  
 def produtoscadastro():
     print("Funcionando")
@@ -24,17 +25,15 @@ def produtos():
     print("Funcionando")
     #Fazer lista de produtos adicionados, e caso não tenha nada, exibir 
 
-
-
 def opcoes():
-    opcao = True
+    roda = True
     print("1- Cadastro de produtos")
     print("2- Produtos")
     print("3- Encerrar cúpula \n")
-    while (opcao):
+    while (roda):
         try:
             opcao = int(input("Digite uma opção: "))
-            if (opcao <= 0 or opcao > 3):
+            if (opcao < 1 or opcao > 3):
                 raise VerificaError
         except ValueError:
             print("ERRO: O valor informado não é um número \n")
@@ -44,25 +43,27 @@ def opcoes():
             if (opcao == 1):
                 produtoscadastro() 
                 #Por enquanto deixar = false, mas depois chamar o incio do programa para ele escolher o produto cadastrado
-                opcao = False
+                roda = False
             elif (opcao == 2):
                 produtos()
                 # Deixar = false, pois quando ele escolher o produto, printa o necessario para ele e finaliza o programa 
-                opcao = False
+                roda = False
             elif (opcao == 3):
                 print("Cúpula desligada !")
-                opcao = False
+                roda = False
         
 
+# VAI FICAR TUDO SOBRE 
 
 def menu():
     print("BEM VINDO(A) A \n")#COLOCAR O NOME DA CUPULA
 
     nome = input("Digite seu nome: ")
     hora(nome)
+    opcoes()
 
 
 
-opcoes()
+menu()
 
 
